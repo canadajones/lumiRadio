989c06b53d79bc717b4c94490baf2189417d19fe
Canada Jones <canadajones68@gmail.com>
2024-05-08 13:31:58 +0200

    Fixed the issue. Had a conditional error where I had an "else" block that was supposed to be nonconditional

--- byers/src/commands/chirp.rs
+++ byers/src/commands/chirp.rs
@@ -2,8 +2,8 @@ use poise::CreateReply;
 use crate::event_handlers::message::update_activity;
 use crate::prelude::*;
 
-use rand::prelude::*;
-use tracing::info;
+use rand::{thread_rng, Rng};
+
 
 /// Chirps back at the person who chirps
 #[poise::command(slash_command)]
@@ -13,30 +13,25 @@ pub async fn chirp(ctx: ApplicationContext<'_>) -> Result<(), Error> {
         update_activity(ctx.data, ctx.author().id, ctx.channel_id(), guild_id).await?;
     }
 
-    info!("chirping!");
     // if author is @canadajones68
     if ctx.author().id == 329162131096338434 {
-
-        info!("oh hai can");
-
-
-        info!("randval obtained");
-        if rand::random() {
-            ctx.send(
-                CreateReply::default()
-                .content("chorp")
-            )
-            .await?;
+        let rand_value = {
+            let mut rng = thread_rng();
+            rng.gen_bool(0.1)
+        };
+        
+        if rand_value {
+            ctx.send(CreateReply::default().content("chorp")).await?;
+            return Ok(());
         }
     }
-    else {
-        ctx.send(
-            CreateReply::default()
-                .content("chirp chirp")
-        )
-        .await?;
-    }
-    info!("i has now replied to the chirp");
+    
+    ctx.send(
+        CreateReply::default()
+            .content("chirp chirp")
+    )
+    .await?;
+    
 
     Ok(())
 }
