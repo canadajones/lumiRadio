079815e43127cbb2e413b21ddae5fcdd5580c4ea
cozyGalvinism <jean@der-capta.in>
2024-05-23 15:49:38 +0200

    Fix broken song requests

--- judeharley/examples/playground.rs
+++ judeharley/examples/playground.rs
@@ -1,2 +1,10 @@
+use judeharley::Decimal;
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let db = judeharley::connect_database("postgres://byers:byers@localhost/byers").await.unwrap();
+
+    let songs = judeharley::Songs::search("zehanpuryu", &db).await.unwrap();
+
+    println!("{:#?}", songs);
+}
